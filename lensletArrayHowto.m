16a14bff0bd19582291b111f53e67a0655eb18e6
Rodolphe Conan <rconan@mso.anu.edu.au>
2011-09-01 22:21:56 +1000

    NEW SOURCE FOCAL LENGTH SETTING The source.objectiveFocalLength property replace the telescope.focalDistance property All scripts need to be updated

--- lensletArrayHowto.m
+++ lensletArrayHowto.m
@@ -4,7 +4,7 @@
 %% A simple 10x10 lenslet array
 nlenslet = 10;
 nPx = 60;
-la = lensletArray(nlenslet)
+la = lensletArray(nlenslet);
 % a circular pupil
 ngs = source.*utilities.piston(nPx);
 % a circular pupil propagated throught the lenslet array
@@ -17,29 +17,29 @@ la.imageletsListener.Enabled = true;
 %% changing the sampling:
 % nyquistSampling=1 corresponds to 2 pixels per fwhm
 la.nyquistSampling = 0.5;
-+ngs
++ngs;
 
 %% increasing the field of view
 % fieldStopSize is given in units of fwhm, for example for a nXn input wave
 % and nyquistSampling=1, the default value of fieldStopSize is
 % (n/nLenslet)/2
 la.fieldStopSize = 6;
-+ngs
++ngs;
 
 %% back to original sampling and field of view
 la.nyquistSampling = 1;
 la.fieldStopSize = 3;
-+ngs
++ngs;
 
 %% a random aberration
 zern = zernike(5:6,'resolution',nPx);
-zern.c = 4*rand(2,1);
+zern.c = 2*ngs.wavelength*rand(2,1);
 ngs = ngs.*zern*la;
 
 %% a random aberration function
 for k=1:100
     o = (k-1).*2*pi/99;
-    zern.c = 4.*[cos(o);sin(o)];
+    zern.c = 2*ngs.wavelength.*[cos(o);sin(o)];
     +ngs;
     drawnow
 end
@@ -56,9 +56,9 @@ colorbar('location','NorthOutside')
 %% sum stacked waves
 zern = zernike(4,'resolution',nPx);
 zern.lex = false;
-zern.c = 6*rand;
+zern.c = 0.5*ngs.wavelength*rand;
 ngs1  = source.*zern;
-zern.c = -6*rand;
+zern.c = -0.5*ngs.wavelength*rand;
 ngs2  = source.*zern;
 la.sumStack = true;
 ngs = source.*cat(3,ngs1.wave,ngs2.wave)*la;
@@ -74,11 +74,12 @@ atm = atmosphere(2.2e-6,1.5,25,...
 tel = telescope(30,...
     'fieldOfViewInArcMin',2,...
     'resolution',nPx*3,...
-    'samplingTime',1/100)
+    'samplingTime',1/100);
 tel = tel+atm;
+figure
 imagesc(+tel)
 %%
-ngs = source('wavelength',2.2e-6).*tel*la;
+ngs = source('wavelength',photometry.K).*tel*la;
 imagesc( la )
 la.imageletsListener.Enabled = true;
 for k=1:50
@@ -88,8 +89,8 @@ for k=1:50
 end
 %%
 % a simple Laser Guide Star
-lgs = source('height',[89.6,90,90.4].*1e3,'wavelength',2.2e-6); 
-tel.focalDistance = 90e3;
+lgs = source('height',[89.6,90,90.4].*1e3,'wavelength',photometry.K); 
+set(lgs,'objectiveFocalLength',90e3)
 lgs = lgs.*tel*la;
 for k=1:50
     +tel;
